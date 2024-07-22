9e8ac585586a7a2fbded4848a6f7f4aae94a9428
Ruben <rubenrdc2003@gmail.com>
2024-07-22 11:51:34 -0300

    Se agrega método de generación de Icono Básico en interfaz Utilities.

--- src/main/java/com/rubenrdc/pcbuilder/IGU/interfaces/Utilities.java
+++ src/main/java/com/rubenrdc/pcbuilder/IGU/interfaces/Utilities.java
@@ -1,6 +1,8 @@
 package com.rubenrdc.pcbuilder.IGU.interfaces;
 
+import java.net.MalformedURLException;
 import java.util.List;
+import javax.swing.ImageIcon;
 
 /**
  *
@@ -21,5 +23,21 @@ public interface Utilities {
         javax.swing.table.DefaultTableModel model = (javax.swing.table.DefaultTableModel) table.getModel();
         model.setRowCount(0);
         table.setModel(model);
+    }
+
+    public static ImageIcon generateImageIcon(String path) {
+        if (path != null) {
+            if (!path.isEmpty()) {
+                try {
+                    //Carga la imagen si es que existe caso contrario lanza una excepcion.
+                    java.net.URL imgURL = new java.net.URL(path);
+                    return new ImageIcon(imgURL);
+                } catch (MalformedURLException ex) {
+                    System.out.println("19 - " + ex);
+                }
+            }
+        }
+        //return new ImageIcon("src/main/java/com/rubenrdc/pcbuilder/miselaneos/icon-image-not-found.jpg");
+        return null;
     }
 }
